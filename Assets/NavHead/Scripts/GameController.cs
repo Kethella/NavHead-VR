563b86e42af5ecbdb2c0c2bd7be58ea877dddbb2
kc029 <kc029@hdm-stuttgart.de>
2025-03-27 10:52:16 +0100

    add virtual room

---
+++ Assets/NavHead/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// This script manages interaction through head movements.
+public class gameController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
